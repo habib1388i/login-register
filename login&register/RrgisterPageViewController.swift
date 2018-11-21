1727b72ff44c259cf91c8e479359e984a890c87c
Muhammad hidayatullah <muhammadhabibhidayatullah@macbook-pro-muhammad-373.local>
2018-11-21 23:34:42 +0700

    Initial Commit

---
+++ login&register/RrgisterPageViewController.swift
@@ -0,0 +1,57 @@
+//
+//  RrgisterPageViewController.swift
+//  login&register
+//
+//  Created by Muhammad Habib hidayatullah on 21/11/18.
+//  Copyright © 2018 habdev. All rights reserved.
+//
+
+import UIKit
+
+class RrgisterPageViewController: UIViewController {
+
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var repeatpasswordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: AnyObject) {
+    
+    
+    
+        let userEmail = userEmailTextField.text;
+        let userPassword = us
+        
+        
+    
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
